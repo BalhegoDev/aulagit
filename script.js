d5cf0c0850c7a2d2c292548492e8cc23f08333c5
Bros12Lek <adgamesbalhego@hotmail.com>
2024-09-04 16:14:33 -0300

    Voltei o código para a primeira versão

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-/*Comandos GIT
-git init
-git add .
-git status
-git config --local user.email "seuemail@email.com"
--- Nos computadores pessoais, usar o --global
-git config --local user.name "seu_usuario"
-git commit -m "criei o arquivo script.js"
-
-git remote add <nome variável> <link do repositório>
-ex: git remote add origin https://github.com/Bros12Lek/aulagit.git
-
-git push <nome variável>
-ex: git push origin
-
-git push --set-upstream <nome variável> <branch>
-ex: git push --set-upstream origin master
-*/
-
 let n1 = 10;
 let n2 = 30;
 const soma = n1 + n2;
--- teste.js
+++
@@ -1,7 +0,0 @@
-function retornaElementos(array){
-    for(let i = 0; i < array.length; i++){
-        console.log(array[i]);
-    }
-}
-
-retornaElementos(["abacaxi",4,7,6,4,"alfredo", "adriano"])
